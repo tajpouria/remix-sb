29f2ba3f0988c88763dff564ab258f15fe741cdf
tajpouria <pouria.tajdivand@betconstruct.com>
2021-11-22 16:47:24 +0400

    feat: wip fund me br

---
+++ fund_me_brownie/contracts/FundMe.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeedAddr) public {
+        priceFeed = AggregatorV3Interaface(_priceFeedAddr);
+    }
+
+    function priceFeedVersion() public view returns (uint256) {
+        return priceFeed.version();
+    }
+}
