a9b1b9c139e1a9c129c2a666eba198cab1dd7160
tajpouria <tajpouria.dev@gmail.com>
2021-11-28 01:49:22 +0400

    fix: add fme contract

--- fund_me_brownie/contracts/FundMe.sol
+++ fund_me_brownie/contracts/FundMe.sol
@@ -5,22 +5,30 @@ pragma solidity >=0.8.0;
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    int256 public constant minUsdThreshold = 5 * (10**18);
+    uint256 public constant minUsdThreshold = 5 * (10**18);
 
     address[] funders;
-    mapping(address => int256) fundersDict;
+    mapping(address => uint256) fundersDict;
 
     address owner;
     AggregatorV3Interface public priceFeed;
 
-    constructor(address _priceFeedAddr) public {
+    constructor(address _priceFeedAddr) {
         owner = msg.sender;
         priceFeed = AggregatorV3Interface(_priceFeedAddr);
     }
 
-    function fund() public {
+    modifier onlyOwner() {
         require(
-            ethToUsd(msg.value) < minUsdThreshold,
+            msg.sender == owner,
+            "Only contract's owner can do the withdrawal"
+        );
+        _;
+    }
+
+    function fund() external payable {
+        require(
+            ethToUsd(msg.value) > minUsdThreshold,
             "You have to invest at least 5 usd please stake more"
         );
 
@@ -28,16 +36,26 @@ contract FundMe {
         funders.push(msg.sender);
     }
 
+    function withdraw() external payable onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+
+        for (uint256 i = 0; i < funders.length; i++) {
+            address f = funders[i];
+            fundersDict[f] = 0;
+        }
+        funders = new address[](0);
+    }
+
     function priceFeedVersion() public view returns (uint256) {
         return priceFeed.version();
     }
 
-    function ethToUsd(int256 _value) private view returns (int256) {
+    function ethToUsd(uint256 _value) private view returns (uint256) {
         return _value * priceFeedLatestRoundRate();
     }
 
-    function priceFeedLatestRoundRate() private view returns (int256) {
+    function priceFeedLatestRoundRate() private view returns (uint256) {
         (, int256 answer, , , ) = priceFeed.latestRoundData();
-        return answer * (10**10);
+        return uint256(answer) * (10**10);
     }
 }
