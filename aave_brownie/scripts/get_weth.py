cf2bc0a7c6b218c9fe52a7326b42feb09d8b62f4
tajpouria <tajpouria.dev@gmail.com>
2021-12-08 22:39:35 +0400

    feat(aave): add get user lending pool date

--- aave_brownie/scripts/get_weth.py
+++ aave_brownie/scripts/get_weth.py
@@ -6,7 +6,9 @@ from scripts.utils import get_owner_account
 
 
 def main():
-    get_weth()
+    # get_weth()
+    lending_pool = get_lending_pool()
+    get_user_account_data(lending_pool)
 
 
 def get_weth():
@@ -14,3 +16,31 @@ def get_weth():
     weth_token.deposit(
         {"from": get_owner_account(), "value": Web3.toWei(0.1, "ether")}
     ).wait(1)
+
+
+def get_user_account_data(lending_pool):
+    (
+        totalCollateralETH,
+        totalDebtETH,
+        availableBorrowsETH,
+        currentLiquidationThreshold,
+        ltv,
+        healthFactor,
+    ) = lending_pool.getUserAccountData(get_owner_account())
+
+    return (
+        float(totalCollateralETH),
+        float(totalDebtETH),
+        float(availableBorrowsETH),
+        float(currentLiquidationThreshold),
+        float(ltv),
+        float(healthFactor),
+    )
+
+
+def get_lending_pool():
+    lendingPoolAddressesProvider = interface.ILendingPoolAddressesProvider(
+        config["networks"][show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_addr = lendingPoolAddressesProvider.getLendingPool()
+    return interface.ILendingPool(lending_pool_addr)
